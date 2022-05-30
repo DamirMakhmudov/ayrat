488d611896277c07f9f3f660a43b972a3f535b81
Damir Makhmudov <samrukov@gmail.com>
2022-05-31 00:20:27 +0300

    test

--- root/root.js
+++ root/root.js
@@ -27,6 +27,66 @@ const vueObject = {
     </q-header>
 
     <q-page-container>
+        <!-- <div class="q-pa-md" style="max-width: 350px"> -->
+        <div class="q-pa-md">
+
+          <q-list bordered class="rounded-borders">
+            <q-expansion-item
+              expand-separator
+              icon="perm_identity"
+              label="Account settings"
+              caption="John Doe"
+            >
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                  eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+      
+            <q-expansion-item
+              expand-separator
+              icon="signal_wifi_off"
+              label="Wifi settings"
+            >
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                  eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+      
+            <q-expansion-item
+              expand-separator
+              icon="drafts"
+              label="Drafts"
+              header-class="text-purple"
+            >
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                  eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+      
+            <q-expansion-item icon="assessment" label="Disabled" disable>
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                  eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+          </q-list>
+        </div>
+
       <!--Карусель-->
       <div class="q-pa-md">
         <q-carousel
